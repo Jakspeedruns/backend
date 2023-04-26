fd633581e65d9758d0e510f222e9b52b1bd6fd80
unknown <yopiewithaj@gmail.com>
2023-04-25 22:42:14 -0600

    first steps: api call and inserting
    api-call.ts makes an api call to speedrun.com, and saves locally it to a .json file.
    read-and-insert.ts creates a table and saves data to it from the .json file

---
+++ test/api-call.ts
@@ -0,0 +1,27 @@
+const request = require('request');
+const fs = require('fs');
+
+//Requesting runs as a json and writing to file
+
+//build strings for paths/filename
+let d = new Date();
+let dateYMD = "" + d.getFullYear() + (d.getMonth()+1) + d.getDate();
+
+const folderpath = './test/jsonblob/';
+let fn = '-jak1.json';
+let filename = dateYMD + fn;
+
+
+let url = 'https://www.speedrun.com/api/v1/runs?game=xkdk4g1m&max=10';
+
+//make the request
+request(url, (err, res, body) => {
+    if (err) { return console.log(err); }
+    
+    //write body to filestream
+    let stream;
+    stream = fs.createWriteStream(folderpath + filename);
+    stream.write(body);
+
+});
+//todo - turn into class with methods for pagination
---
+++ test/read-and-insert.ts
@@ -0,0 +1,91 @@
+const fs = require('fs');
+const db = require('better-sqlite3')('./.wrangler/state/d1/DB.sqlite3');
+
+//create temprun table, prepare insert template
+createTempRuns();
+const stmt = db.prepare(`INSERT INTO TempRuns(  
+    [SRId],
+    [GameId],
+    [CatId],
+    
+    [TimeSecs],
+    [PlatformId],
+    [Emulated],
+    [RegionId],
+    [VideoLink],
+
+    [SubmitDate],
+    [Status],
+    [Examiner],
+    [VerifyDate],
+    
+    
+    [Variables])
+    VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`
+    );
+
+
+//strings for paths/filename
+const folderpath = './test/jsonblob/';
+let filename = '2023425-jak1.json';         //filename hardcoded for testing
+
+//a list we will append with values from a run's json
+let fields = [];
+
+fs.readFile(folderpath+filename, (err, data) => {
+    if (err) throw err;
+    
+    //parse the json data, access the .data field, not .pagination
+    let blob = JSON.parse(data).data;           // fix after doing pagination 
+
+    //loop through each run of the json data
+    //for (let i=0; i < blob.length; i++) {          
+        //console.log(blob[i])        //each run object
+
+        fields.push(blob[1].id)
+        fields.push(blob[1].game)
+        fields.push(blob[1].category)
+        fields.push(blob[1].times.realtime_t)                   //time to string logic eventually
+        fields.push(blob[1].system.platform)
+        fields.push(blob[1].system.emulated.toString())         //need to convert to int
+        fields.push(blob[1].system.region)
+        fields.push(blob[1].videos.links[0].uri)
+        //fields.push(blob[1].comment.toString())                //need to handle javascript nulls
+        fields.push(blob[1].date)
+        fields.push(blob[1].status.status)
+        fields.push(blob[1].status.examiner)
+        fields.push(blob[1].status['verify-date'])              //make date formats consistent
+        fields.push(JSON.stringify(blob[1].values))
+    
+        console.log(fields)
+        stmt.run(fields)
+    //}
+    
+});
+
+function createTempRuns() {
+const createStmt = `CREATE TABLE IF NOT EXISTS TempRuns(
+    RunId          INTEGER PRIMARY KEY,
+    SRId           TEXT,
+    GameId         TEXT,
+    CatId          TEXT,
+    Time           TEXT,
+    TimeSecs       REAL,
+    PlatformId     TEXT,
+    Emulated       TEXT,
+    RegionId       TEXT,
+    VideoLink      TEXT,
+    Comment        TEXT,
+    SubmitDate     TEXT,
+    Status         TEXT,
+    Examiner       TEXT,
+    VerifyDate     TEXT,
+    RowCreatedDate TEXT,
+    AlgPoint       INTEGER,
+    Variables      TEXT
+    );`
+
+db.exec(createStmt);
+}
+
+
