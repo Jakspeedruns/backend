0eaf46e419346ca55710d472a8585554824ba7f8
Tyler Wilding <xtvaser@gmail.com>
2024-02-17 22:56:31 -0500

    api: fix the cache

--- src/routes/v1/highscores.ts
+++ src/routes/v1/highscores.ts
@@ -1,29 +1,14 @@
 import { Env } from "../..";
-import { Highscores, getHighscores } from "../../storage/d1";
-
-interface HighscoreCacheEntry {
-  highscores: Highscores[];
-  timestamp: number;
-}
-
-const highscoreCache = new Map<string, HighscoreCacheEntry>();
+import { getHighscores } from "../../storage/d1";
 
 export const ListHighscores = async (request: any, env: Env, ctx: ExecutionContext) => {
   // TODO - handle no highscore ID
   const highscoreId = request.params.highscoreId;
 
-  let highscores;
-  if (highscoreCache.has(highscoreId) && highscoreCache.get(highscoreId).timestamp > Date.now() - 60 * 60 * 1000) {
-    highscores = highscoreCache.get(highscoreId).highscores;
-  } else {
-    highscores = await getHighscores(env.DB, highscoreId);
-    highscoreCache.set(highscoreId, {
-      highscores,
-      timestamp: Date.now(),
-    });
-  }
+  const highscores = await getHighscores(env.DB, highscoreId);
   // TODO - finer grained CORS up above (middleware or something)
   const headers = {
+    "Cache-Control": "max-age=6000",
     "Content-type": "application/json",
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Methods": "GET",
