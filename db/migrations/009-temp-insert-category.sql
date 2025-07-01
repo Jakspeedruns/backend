21b13c3c3da7bb3a79ba1c3e69a6fea53b15968e
Grant Saylor <55508592+G3catcher@users.noreply.github.com>
2025-07-01 16:19:22 -0400

    Update 009-temp-insert-category.sql
    Added all console categories

--- db/migrations/009-temp-insert-category.sql
+++ db/migrations/009-temp-insert-category.sql
@@ -1,52 +1,114 @@
-INSERT OR IGNORE INTO Category(SRId, CatName, GameID, IL)
-    VALUES    ('5dw8r40d', 'No LTS', 'xkdk4g1m', 0),
-              ('rkllg7qk', 'No FCS', 'xkdk4g1m', 0),
-              ('jdrqj0k6', 'Any%', 'xkdk4g1m', 0),
-              ('02qgyzdy', '100%', 'xkdk4g1m', 0),
-              ('824x53d5', 'Orbless', 'xkdk4g1m', 0),
-              ('02qyyydy', 'No Major Skips', 'xkdk4g1m', 0),
-              ('02qjn8pd', 'All Flies', 'xkdk4g1m', 0),
-              ('q25oj1vk', 'All Orbs', 'xkdk4g1m', 0),    --Note there is another 'all orbs' category mkevyjx2.
-              ('5dw75yek', 'All Power Cells IL', 'xkdk4g1m', 1),     --IL Category
-              ('wk6v71qd', '100% IL', 'xkdk4g1m', 1),     --IL Category
-              ('wdmze42q', 'Any%', 'ok6qlo1g', 0),
-              ('xd1rxxrk', 'Any% Hoverless', 'ok6qlo1g', 0),
-              ('mkeon9d6', 'All Missions', 'ok6qlo1g', 0),
-              ('7dg8q424', '100%', 'ok6qlo1g', 0),
-              ('wkpj7vkr', 'Any % All Orbs', 'ok6qlo1g', 0),
-              ('vdo0jodp', 'Any% Hero Mode', 'ok6qlo1g', 0),
-              ('9d8p1qkn', 'Any%', 'nj1nww1p', 0),
-              ('9kvp50kg', 'Any% Hero Mode', 'nj1nww1p', 0),
-              ('5dwj0n0k', 'Any% No OoB', 'nj1nww1p', 0),
-              ('xd1r98k8', 'All Missions', 'nj1nww1p', 0),
-              ('zd30nndn', '100%', 'nj1nww1p', 0),
-              ('jdzw79vd', 'Any% All Orbs', 'nj1nww1p', 0),
-              ('5dwp5nkg', 'Any%', 'n268l71p', 0),
-              ('wk6j7rd1', '100%', 'n268l71p', 0),
-              ('xd1ezxr2', 'Any% Debug Mode', 'n268l71p', 0),
-              ('5dwjogkg', 'Any%', 'xldeq5d3', 0),
-              ('wk6p9ed1', '100%', 'xldeq5d3', 0),
-              ('9kvm8xek', 'All Cups', 'xldeq5d3', 0),
-              ('wkp096v2', 'All Story Arenas', 'xldeq5d3', 0),
-              ('n2yqweko', 'Any% Hero Mode', 'xldeq5d3', 0),
-              ('7kjvxgk3', '100% Hero Mode', 'xldeq5d3', 0),
-              ('rklxpwkn', 'Any%', 'n26847dp', 0),
-              ('vdo5zqyk', 'No TMS', 'n26847dp', 0),
-              ('9kvxmj2g', 'All Missions', 'n26847dp', 0),
-              ('ndxjgj2q', '101%', 'n26847dp', 0),
-              ('q25oywko', 'Any%', 'm9dogm6p', 0),
-              ('xk9eyjy2', 'Hero Mode Any%', 'm9dogm6p', 0),
-              ('xk99ngk0', 'Hero Mode No SPS', 'm9dogm6p', 0),
-              ('z279ood0', '101%', 'm9dogm6p', 0),
-              ('jdrqz0k6', '100%', 'm9dogm6p', 0),
-              ('zdnx3y92', 'Jak 1', 'kdkz25qd', 0),    --6njqpm5l
-              ('q253p1vk', 'Jak 2', 'kdkz25qd', 0),    --kn0zey08
-              ('jdre0n5k', 'Jak 3', 'kdkz25qd', 0),    --ql649vw8
-              ('jdz74ngk', 'Jak X', 'kdkz25qd', 0),    --ylpmkpjl
-              ('02qpxryd', 'Daxter', 'kdkz25qd', 0),   --0nwky65n
-              ('8241o8w2', 'TLF', 'kdkz25qd', 0),      --789je95n
-              ('xd1lxwrk', 'Multi-Games', 'kdkz25qd', 0),    --2lggkd6l
-              ('9d8xw572', 'Jak 2 Flash Game', 'kdkz25qd', 0),    --wlex31r8
-              ('jdz68rr2', 'Bingo', 'kdkz25qd', 0),
-              ('wkp953g2', 'Randomizer', 'kdkz25qd', 0)
+INSERT OR IGNORE INTO Category(SRId, CatName, GameID, IL, CatExt, Diff)
+    VALUES    --Jak 1
+              ('5dw8r40d', 'No LTS', 'xkdk4g1m', 0, 0, 2),
+              ('rkllg7qk', 'No FCS', 'xkdk4g1m', 0, 0, 3),
+              ('jdrqj0k6', 'Any%', 'xkdk4g1m', 0, 0, 4),
+              ('02qgyzdy', '100%', 'xkdk4g1m', 0, 0, 2),
+              ('824x53d5', 'Orbless', 'xkdk4g1m', 0, 0, 3),
+              ('02qjn8pd', 'All Flies', 'xkdk4g1m', 0, 0, 3),
+              ('q25oj1vk', 'All Orbs', 'xkdk4g1m', 0, 0, 3),
+              --Jak 1 Ext
+              ('vdopw3o2', 'NMS', '3dxkp3g1', 0, 1, 2),    --013g8mxl
+              ('vdopw3o2', 'No LTS Dark Glitch', '3dxkp3g1', 0, 1, 3),    --013g8mxl
+              ('vdopw3o2', 'No LTS No Damage', '3dxkp3g1', 0, 1, 4),    --013g8mxl
+              ('02qeyo72', 'Debug Any%', '3dxkp3g1', 0, 1, 1),    --5lejr8k1
+              ('02qeyo72', 'Debug 100%', '3dxkp3g1', 0, 1, 1),    --0q5rn821
+              ('9d8516wd', '100% Hub 1', '3dxkp3g1', 0, 1, 0),    --zqo4pnx1
+              ('9d8516wd', '100% Hub 2', '3dxkp3g1', 0, 1, 0),    --013g8rxl
+              ('9d8516wd', '100% Hub 3', '3dxkp3g1', 0, 1, 0),    --rqv8pd61
+              ('xd1w9ez2', 'NG+ Lava Walk', '3dxkp3g1', 0, 1, 0),    --4lxnp441
+              ('xd1w9ez2', 'NG+ No Lava Walk', '3dxkp3g1', 0, 1, 0),    --814zydvl
+              ('xd1w9ez2', 'NG+ All Flies', '3dxkp3g1', 0, 1, 0),    --p127dw4q
+              ('xd1w9ez2', 'NG+ All Orbs', '3dxkp3g1', 0, 1, 0),    --12vv2y4q
+              ('jdzr18r2', 'Low Eco', '3dxkp3g1', 0, 1, 3),    --z19nmr8q
+              ('jdzr18r2', '9 Eco', '3dxkp3g1', 0, 1, 0),    --81p9pn8q
+              ('jdzr18r2', '10 Eco', '3dxkp3g1', 0, 1, 0),    --p127dz4q
+              ('9d8516qd', 'Orbless Max%', '3dxkp3g1', 0, 1, 3),   
+              ('rkl04jwk', '1 X Press', '3dxkp3g1', 0, 1, 3),    
+              ('9kvr5me2', 'All Flies and Orbs', '3dxkp3g1', 0, 1, 3),    
+              ('zd35n6ek', '52 Pickup', '3dxkp3g1', 0, 1, 1),    
+              ('ndxrz1jd', 'Kill All Animals', '3dxkp3g1', 0, 1, 1),   
+              ('w20ejyzd', 'All Cutscenes', '3dxkp3g1', 0, 1, 2),   
+              ('wkpg6vvk', 'Muse%', '3dxkp3g1', 0, 1, 1),   
+              ('7dg1l94d', 'No BS', '3dxkp3g1', 0, 1, 2),    
+              ('mkew3r9k', 'All Flies Low Collectables', '3dxkp3g1', 0, 1, 2),    
+              --Jak 2
+              ('wdmze42q', 'Any%', 'ok6qlo1g', 0, 0, 2),
+              ('xd1rxxrk', 'Any% Hoverless', 'ok6qlo1g', 0, 0, 3),
+              ('mkeon9d6', 'All Missions', 'ok6qlo1g', 0, 0, 3),
+              ('7dg8q424', '100%', 'ok6qlo1g', 0, 0, 3),
+              ('wkpj7vkr', 'Any % All Orbs', 'ok6qlo1g', 0, 0, 2),
+              ('vdo0jodp', 'Any% Hero Mode', 'ok6qlo1g', 0, 0, 2),
+              --Jak 2 Cat Ext
+              ('9kvr5lo2', 'All Missions Act 1', 'v1pxqgm6', 0, 1, 0),    --klrgpkjq
+              ('9kvr5lo2', 'All Missions Act 2', 'v1pxqgm6', 0, 1, 0),    --21d0z84q
+              ('9kvr5lo2', 'All Missions Act 3', 'v1pxqgm6', 0, 1, 0),    --5q8y6vgl
+              ('rkl04zrk', 'Any% No Damage', 'v1pxqgm6', 0, 1, 4),    --5lm5k3jq
+              ('rkl04zrk', 'Any% Mirrored', 'v1pxqgm6', 0, 1, 3),    --81wyp2oq
+              ('w20ej58d', 'Any% 1/3', 'v1pxqgm6', 0, 1, 0),    --8104znwl
+              ('w20ej58d', 'Any% 2/3', 'v1pxqgm6', 0, 1, 0),    --9qj30mel
+              ('w20ej58d', 'Any% 3/3', 'v1pxqgm6', 0, 1, 0),    --jq6d0o31
+              ('xd1w9q72', 'Debug', 'v1pxqgm6', 0, 1, 1),    
+              ('9d851n6d', 'Gunless', 'v1pxqgm6', 0, 1, 3),  
+              ('zd35nr8k', 'Ashelin Stalker', 'v1pxqgm6', 0, 1, 2),   
+              ('vdopwny2', 'All Open Orbs (Endgame)', 'v1pxqgm6', 0, 1, 2),   
+              ('n2yrw6zd', 'Developer Picture', 'v1pxqgm6', 0, 1, 1), 
+              ('wdmmg75d', 'Praxis 1', 'v1pxqgm6', 0, 1, 0),
+              ('ndxrz05d', 'Im Gonna Kill Praxis', 'v1pxqgm6', 0, 1, 1),
+              --Jak 3
+              ('9d8p1qkn', 'Any%', 'nj1nww1p', 0, 0, 3),
+              ('9kvp50kg', 'Any% Hero Mode', 'nj1nww1p', 0, 0, 2),
+              ('5dwj0n0k', 'Any% No OoB', 'nj1nww1p', 0, 0, 2),
+              ('xd1r98k8', 'All Missions', 'nj1nww1p', 0, 0, 3),
+              ('zd30nndn', '100%', 'nj1nww1p', 0, 0, 3),
+              ('jdzw79vd', 'Any% All Orbs', 'nj1nww1p', 0, 0, 2),
+              --Jak 3 Cat Ext
+              ('q255rn82', 'All Missions Act 1', '76r3eg46', 0, 1, 0),    --81wyz5vq
+              ('q255rn82', 'All Missions Act 2', '76r3eg46', 0, 1, 0),    --81wyz5vq
+              ('q255rn82', 'All Missions Act 3', '76r3eg46', 0, 1, 0),    --81wyz5vq
+              ('q255rn82', 'All Missions Act 1 NG+', '76r3eg46', 0, 1, 0),    --81wyz5vq
+              ('q255rn82', 'All Missions Act 2 NG+', '76r3eg46', 0, 1, 0),    --81wyz5vq
+              ('q255rn82', 'All Missions Act 3 NG+', '76r3eg46', 0, 1, 0),    --81wyz5vq
+              ('jdzr1mv2', 'No OoB NG+', '76r3eg46', 0, 1, 0),    
+              ('jdrwj5xk', 'All Bosses NG+', '76r3eg46', 0, 1, 2),    
+              ('824q57gk', 'All Satellites NG+', '76r3eg46', 0, 1, 1), 
+              ('02qeynp2', 'Jetboardless NG+', '76r3eg46', 0, 1, 2),  
+              ('z2758qgk', 'Debug', '76r3eg46', 0, 1, 1),   
+              ('zd3m5682', 'Hero Mode Hoverless', '76r3eg46', 0, 1, 2),   
+              ('zdnoejq2', 'All Open Orbs', '76r3eg46', 0, 1, 2),    
+              ('q25plowd', 'All Open Orbs (Post Game)', '76r3eg46', 0, 1, 2),   
+              ('8243mwnd', 'Gunless (Fresh File)', '76r3eg46', 0, 1, 3),    --81wyz5vq
+              ('8243mwnd', 'Gunless (Hero Mode)', '76r3eg46', 0, 1, 3),    --81wyz5vq
+              ('7dgjoz4d', 'No OoB Restricted', '76r3eg46', 0, 1, 0),   
+              --Jak X
+              ('5dwjogkg', 'Any%', 'xldeq5d3', 0, 0, 3),
+              ('wk6p9ed1', '100%', 'xldeq5d3', 0, 0, 4),
+              ('9kvm8xek', 'All Cups', 'xldeq5d3', 0, 0, 2),
+              ('n2yqweko', 'Any% Hero Mode', 'xldeq5d3', 0, 0, 3),
+              ('7kjvxgk3', '100% Hero Mode', 'xldeq5d3', 0, 0, 3),
+              --Jak X Ext
+              ('mke71762', 'All Story Arenas', '46w28796', 0, 1, 0), 
+              ('5dwx3xl2', 'All Cups & Qualifiers (Skips)', '46w28796', 0, 1, 0),    --zqovre41
+              ('5dwx3xl2', 'All Cups & Qualifiers (No Skips)', '46w28796', 0, 1, 0),    --01365ky1
+              ('wk64q4xd', '24 Tracks (Skips)', '46w28796', 0, 1, 0),    --klr2yk01
+              ('wk64q4xd', '24 Tracks (No Skips)', '46w28796', 0, 1, 0),    --gq7xr3rl
+              ('7kj4o5nk', 'All Tours (Skips)', '46w28796', 0, 1, 0),    --le26y36l
+              ('7kj4o5nk', 'All Tours (No Skips)', '46w28796', 0, 1, 0),    --q5v9wxvl
+              --Daxter
+              ('rklxpwkn', 'Any%', 'n26847dp', 0, 0, 3),
+              ('vdo5zqyk', 'No TMS', 'n26847dp', 0, 0, 2),
+              ('9kvxmj2g', 'All Missions', 'n26847dp', 0, 0, 3),
+              ('ndxjgj2q', '101%', 'n26847dp', 0, 0, 2),
+              --Daxter Cat Ext
+              ('rklm13rd', 'All Masks', '4d7ng7r6', 0, 1, 2),
+              --TLF
+              ('q25oywko', 'Any%', 'm9dogm6p', 0, 0, 2),
+              ('xk9eyjy2', 'Hero Mode Any%', 'm9dogm6p', 0, 0, 2),
+              ('xk99ngk0', 'Hero Mode No SPS', 'm9dogm6p', 0, 0, 2),
+              ('z279ood0', '101%', 'm9dogm6p', 0, 0, 4),
+              ('jdrqz0k6', 'Max% No Hero Mode', 'm9dogm6p', 0, 0, 4),
+              --Trifecta
+              ('5dwp5nkg', 'Any%', 'n268l71p', 0, 0, 3),
+              ('wk6j7rd1', '100%', 'n268l71p', 0, 0, 4),
+              ('xd1ezxr2', 'Any% Debug Mode', 'n268l71p', 0, 0, 1)
               ;
